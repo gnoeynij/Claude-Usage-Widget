efd65763aa84a6e6e08ab8cd0a424615309542a6
choi jinyeong <cjy1521@gmail.com>
2026-07-28 10:17:03 +0900

    chore(release): v2.4.9

--- src/state/store.ts
+++ src/state/store.ts
@@ -247,7 +247,7 @@ const [store, setStore] = createStore<StoreShape>({
   syncing: false,
   syncError: null,
   errorCode: null,
-  version: "2.4.8",
+  version: "2.4.9",
   tickMinute: 0,
   tickSecond: 0,
   lifetimeCost: 0,
